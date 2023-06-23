b32db831d24ed3076fb18a266a681f532d597f79
Code Sandbox <hello@codesandbox.io>
2023-06-23 10:35:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6100844abc285caeab4453596a583777.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df628ad9b945c732863297739483037",
+    "url": "https://niteshkoli.github.io/csb-2xgzjx/index.html"
+  },
+  {
+    "revision": "ee9e355d3438769b7034",
+    "url": "https://niteshkoli.github.io/csb-2xgzjx/static/js/2.88831c34.chunk.js"
+  },
+  {
+    "revision": "0468d4fad5f132c609d4",
+    "url": "https://niteshkoli.github.io/csb-2xgzjx/static/js/main.2f1a1aca.chunk.js"
+  },
+  {
+    "revision": "2b80c2acc66a3a9c53d7",
+    "url": "https://niteshkoli.github.io/csb-2xgzjx/static/js/runtime-main.ed56f4bb.js"
+  }
+]);
